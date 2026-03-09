519a5522e7fa92daaeb20ac269cd32af93db30a1
Mehmet Konukçu <mehmetext68@gmail.com>
2026-03-09 14:09:17 +0300

    feat: enhance application with global exception handling and validation, and improve Swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { RedisModule } from '@nestjs-modules/ioredis';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { RoomModule } from './modules/room/room.module';
 
 @Module({
@@ -37,6 +39,11 @@ import { RoomModule } from './modules/room/room.module';
     RoomModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,48 @@
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { BaseResponseInterceptor } from './common/interceptors/base-response.interceptor';
 
 async function bootstrap() {
+  if (!process.env.PORT) {
+    throw new Error('PORT is not set');
+  }
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Kahoot Clone API')
-    .setDescription('Kahoot clone servisleri için API dokümantasyonu')
-    .setVersion('1.0.0')
+    .setTitle('Kahoot Clone')
+    .setDescription('The Kahoot Clone API')
+    .setVersion('1.0')
+    .addBearerAuth()
     .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors) => {
+        const errorMessages = errors.flatMap((error) =>
+          Object.values(error.constraints ?? {}),
+        );
+
+        return new BadRequestException(errorMessages);
+      },
+    }),
+  );
+
+  app.useGlobalInterceptors(new BaseResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
